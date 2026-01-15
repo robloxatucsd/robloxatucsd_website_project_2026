ac71808ae3b2b0deffcaa1c60d62b9be6a0cabc7
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2026-01-14 19:34:55 -0800

    Created basic template for navigation between pages

--- my-app/app/team/page.tsx
+++ my-app/app/team/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Team Page</h1>
       <nav>
         <Link href="/">Home</Link>
         <Link href="/team">Team</Link>
